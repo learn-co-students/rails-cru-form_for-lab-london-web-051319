79149777a21d3a2e981a522fd838672116cf1a99
github username <github email address>
2019-06-09 17:24:30 +0100

    Met all requirements.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,44 @@
+class ArtistsController < ApplicationController
+   before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  def index
+     @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def new
+     @artist = Artist.new
+  end
+
+  def create
+     @artist = Artist.create(artist_params(:name, :bio))
+     redirect_to artist_path(@artist)
+  end
+
+  def edit
+  end
+
+  def update
+     @artist.update(artist_params(:name, :bio))
+     redirect_to artist_path(@artist)
+  end
+
+  def destroy
+     @artist.destroy
+     redirect_to artists_path
+  end
+
+  private
+
+  def set_artist
+     @artist = Artist.find(params[:id])
+  end
+
+  def artist_params(*args)
+     params.require(:artist).permit(args)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/home', to: 'static#home'
+
+  resources :genres, :artists, :songs
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
